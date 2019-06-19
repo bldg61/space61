b06992dcab186cc8f668c976d1e0527777e53d8f
Emily Platzer <emilyplatzer@gmail.com>
2019-06-18 19:15:02 -0600

    Added WIP for csv styleeee

---
+++ flightTracker/flightRecorderForComputers.ino
@@ -0,0 +1,131 @@
+//Flight tracker GPS code
+
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+///MPL3115 section////
+#include <Wire.h>
+#include "SparkFunMPL3115A2.h"
+
+//Create an instance of the object
+MPL3115A2 myPressure;
+///End MPL3115 section ////
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup()
+{
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+  ///Begin MPL3115 section
+  Wire.begin();        // Join i2c bus
+
+  myPressure.begin(); // Get sensor online
+
+  //Configure the sensor
+  myPressure.setModeAltimeter(); // Measure altitude above sea level in meters
+  //myPressure.setModeBarometer(); // Measure pressure in Pascals from 20 to 110 kPa
+
+  myPressure.setOversampleRate(7); // Set Oversample to the recommended 128
+  myPressure.enableEventFlags(); // Enable all three pressure and temp event flags
+  ///End MPL3115 Section ///
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true);
+  }
+  ///Begin MPL3115///
+  float altitude = myPressure.readAltitudeFt();
+  Serial.print(",");
+  // Serial.print(" Altitude(ft):");
+  Serial.print(altitude, 2);
+  Serial.print(",");
+
+  float temperature = myPressure.readTempF();
+  //Serial.print(" Temp(f):");
+  Serial.print(temperature, 2);
+  Serial.print(",");
+
+  float pressure = myPressure.readPressure();
+  //Serial.print("Pressure(Pa):");
+  Serial.print(pressure, 2);
+
+  //float temperature = myPressure.readTemp();
+  //Serial.print(" Temp(c):");
+  //Serial.print(temperature, 2);
+
+
+ Serial.println("\t");
+}
+
+void displayInfo()
+{
+
+  //Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  //Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  //Serial.println();
+  //Serial.print(F("Location: "));
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+}
